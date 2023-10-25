6a87bcb5833b47e41bd10c51ccb850b3ce320f19
Самойленко Максим Андреевич <msamoylenko@fil-it.ru>
2023-10-25 19:21:37 +0300

    init 2

--- test/src/main/java/org/example/App.java
+++ test/src/main/java/org/example/App.java
@@ -7,6 +7,7 @@ package org.example;
 public class App 
 {
     public static void main( String[] args ) {
+        // voice:.idea/1698250882224.wav
         System.out.println( "Hello World!" );
 // voice:.idea/1698250709791.wav        // voice:.idea/1698250665996.wav
     }
